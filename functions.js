0c354d1a2ad09e6a3212b8adbcd736455bdaf7d0
JakobWood0616 <jakestevenwood@gmail.com>
2022-12-03 11:03:19 -0500

    Updated redirect link

--- functions.js
+++ functions.js
@@ -2,5 +2,5 @@ function closeSubmit(){
     var subject = document.getElementById("subject").value;
     var message = document.getElementById("message").value;
     window.location.href = "mailto:jakestevenwood.com?subject="+subject+"&body="+message;
-    location.href = 'https://jakestevenwood.github.io/';
+    location.href = 'https://jakobwood.ca/';
 }
